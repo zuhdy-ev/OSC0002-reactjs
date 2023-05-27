bfec17a9e8426557f11df2d504abede832ec897a
zuhdy-ev <ardhiekov@gmail.com>
2023-05-28 00:06:10 +0700

    playing with reactjs for the first time

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src="../img/react-icon.png" alt="" className="nav--icon_img" />
+            <h3 className="nav--icon_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 01</h4>
+        </nav>
+    )
+}
